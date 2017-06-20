35117049f70979f7bc4c0d6589d2027de496db7b
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-06-20 02:05:52 -0500

    Updated Whitepapers

--- src/pages/whitepapers/whitepapers.ts
+++ src/pages/whitepapers/whitepapers.ts
@@ -10,12 +10,16 @@ export class WhitepapersPage {
 
   whitepapers: Array<{ no: number, title: string, note: string,link:string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.whitepapers = [{no:1,title:"Overview of Security Processes",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Security/AWS_Security_Best_Practices.pdf"},
-    {no:2,title:"Storage Options in the Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
-    {no:3,title:"Fault Tolerant Apps in the cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-building-fault-tolerant-applications.pdf"},
-    {no:4,title:"Overview of AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-overview.pdf"},
-    {no:5,title:"Compliance Whitepaper",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Risk_and_Compliance_Whitepaper.pdf"},
-    {no:6,title:"Architecting for the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Cloud_Best_Practices.pdf"}];
+    this.whitepapers = [{no:1,title:"Security at Scale: Governance in AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Security_at_Scale_Governance_in_AWS_Whitepaper.pdf"},
+    {no:2,title:"Security at Scale: Logging in AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Security_at_Scale_Logging_in_AWS_Whitepaper.pdf"},
+    {no:3,title:"Securing Data at Rest with Encryption",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Securing_Data_at_Rest_with_Encryption.pdf"},
+    {no:4,title:"Development and Test on Amazon Web Services",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Development_Test_Environments.pdf"},
+    {no:5,title:"Operational Checklists for AWS",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Operational_Checklists.pdf"},
+    {no:6,title:"Architecting for the Cloud: Best Practices",note:"sdfsdfsd",link:"https://media.amazonwebservices.com/AWS_Cloud_Best_Practices.pdf"},
+    {no:7,title:"Building Fault-Tolerant Applications on AWS",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Building_Fault_Tolerant_Applications.pdf"},
+    {no:8,title:"Storage Options in the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
+    {no:9,title:"Backup and Recovery Approaches Using Amazon Web Services",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Backup_and_Recovery_Approaches_Using_AWS.pdf"},
+    {no:10,title:"AWS Security Best Practices",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Security/AWS_Security_Best_Practices.pdf"}];
   }
 
   ionViewDidLoad() {
