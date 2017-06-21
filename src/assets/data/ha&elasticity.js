3e12f20ebf883f23f96148b7c962e5a4ba03e3a7
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-06-20 21:51:39 -0500

    Updated HA & Elasticity

---
+++ src/assets/data/ha&elasticity.js
@@ -0,0 +1,109 @@
+{
+    "quiz": {
+        "name": "High Availability & Elasticity",
+        "logo" : "assets/img/VPC.png",
+        "time":"20 minutes"
+    },
+    "questions": [
+         {
+        "Id": 1,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+
+      {
+        "Id": 2,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+    {
+        "Id": 3,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+    {
+        "Id": 4,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+    {
+        "Id": 5,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+    {
+        "Id": 6,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    },
+    {
+        "Id": 7,
+        "Name": "", 
+        "Tag":"ha",
+        "Options": [
+            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
+            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
+            "Explanation":"",
+            "Ref":""
+
+    }
+   
+     ]
+}
