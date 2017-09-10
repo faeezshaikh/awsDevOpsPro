0445df297db8479a30a00f6d472bd92d9fdd6c84
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-09-10 12:06:59 -0500

    Q # 64

--- src/assets/data/exam1.js
+++ src/assets/data/exam1.js
@@ -830,15 +830,16 @@
         ,
         {
             "Id": 64,
-            "Name": "",
-            "Tag": "",
+            "Name": "You run a large number of applications on Amazon EC2 instances. Each application has associated metadata, such as cost center, support contact, and application ID. Many applications usually co-exist on each Amazon EC2 instance, so the amount of metadata per instance can range from 10 to 200 items. The customer wants to be able to quickly access this metadata using an API without logging into the instances. Which of the following options will satisfy their requirements? Choose 2 answers",
+            "Tag": "sec&gov",
             "Options": [
-                { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-                { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-                { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-                { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false }],
-            "Explanation": "",
-            "Ref": ""
+                { "Id": 1055, "QuestionId": 1010, "Name": "Create individual Amazon EC2 tags for each metadata item, and associate them with the Amazon EC2 instances. Access the metadata by using the ec2-describe-instance API call.", "IsAnswer": false },
+                { "Id": 1056, "QuestionId": 1010, "Name": "Create compound Amazon EC2 tags for the metadata items, where multiple items are joined together in individual tags, and associate them with the Amazon EC2 instances. Access the metadata by using the ec2-describe-tags API call.", "IsAnswer": true },
+                { "Id": 1057, "QuestionId": 1010, "Name": "Create a DynamoDB table to hold the metadata, and associate it with the Amazon EC2 instance IDs running the applications. Access the metadata by querying the database via the DynamoDB API.", "IsAnswer": true},
+                { "Id": 1058, "QuestionId": 1010, "Name": "As part of the Amazon EC2 Instance bootstrapping process, add the metadata to the Amazon EC2 user data. Access the metadata by using the ec2-describe-instance API call.", "IsAnswer": false },
+                { "Id": 1059, "QuestionId": 1010, "Name": "As part of the Amazon EC2 instance bootstrapping process, add the metadata to the Amazon EC2 user data. Access the metadata by accessing its loopback address from a management instance in the same VPC.", "IsAnswer": false }],
+            "Explanation": "To maximize the 50 tags that can be assigned to a resource, consider using compound tags. Compound tags combine multiple key-value pairs into a single key paired with a string comprised of multiple pipe delimited ‘values’. By doing this, you might combine the 3 keys “Ownername”, “OwnerPhone”, and “OwnerEmail” into one “OwnerContact” key paired with a pipe delimited string of values for name, phone, and e-mail address, such as: John Doe|1-555-555-1212|jdoe@example.com. The other option is to use DynamoDB to store the metadata.",
+            "Ref": "https://aws.amazon.com/premiumsupport/knowledge-center/ec2-resource-tags/"
         }
         ,
         {
