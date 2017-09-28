2d08089f20de61f79e70f07737cdd0cf22b175e4
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-09-28 15:09:58 -0500

    Sec # 1

--- src/assets/data/exam1.js
+++ src/assets/data/exam1.js
@@ -554,19 +554,7 @@
                 { "Id": 1059, "QuestionId": 1010, "Name": "Define a UserDataHandle for the output of a UserData command that does sanity checking of the application's post-install state and runs integration tests on the state of multiple tiers through the load balancer to the application.", "IsAnswer": true},
                 { "Id": 1060, "QuestionId": 1010, "Name": "Define a UserDataHandle for the output of a CustomResource that does sanity checking of the application's post-install state.", "IsAnswer": false }]
         }
-        ,
-        {
-            "Id": 62,
-            "Name": "You are building a large, multi-tenant SaaS (software-as-a-service) application with a component that fetches data to process from a customer-specific Amazon S3 bucket in their account. How should you ensure that your application follows security best practices and limits risk when fetching data from customer-owned Amazon S3 buckets?",
-            "Tag": "security",
-            "Options": [
-                { "Id": 1055, "QuestionId": 1010, "Name": "Have users create an IAM user with a policy that grants read-only access to the Amazon S3 bucket required by your application, and store the corresponding access keys in an encrypted database that holds their account data.", "IsAnswer": false },
-                { "Id": 1056, "QuestionId": 1010, "Name": "Have users create a cross-account lAM role with a policy that grants read-only access to the Amazon S3 bucket required by your application to the AWS account ID running your production Sass application.", "IsAnswer": true },
-                { "Id": 1057, "QuestionId": 1010, "Name": "Have users create an Amazon S3 bucket policy that grants read-only access to the Amazon S3 bucket required by your application, and securely store the corresponding access keys in the database holding their account data.", "IsAnswer": false },
-                { "Id": 1058, "QuestionId": 1010, "Name": "Have users create an Amazon S3 bucket policy that grants read-only access to the Amazon S3 bucket required by your application and limits access to the public IP address of the SaaS application.", "IsAnswer": false }],
-            "Explanation": "See link for how to delegate access across AWS accounts using IAM roles",
-            "Ref": "http://docs.aws.amazon.com/IAM/latest/UserGuide/tutorial_cross-account-with-roles.html"
-        }
+       
         ,
         {
             "Id": 63,
--- src/assets/data/security&governance.js
+++ src/assets/data/security&governance.js
@@ -5,21 +5,19 @@
         "time":"20 minutes"
     },
     "questions": [
-         {
-        "Id": 1,
-        "Name": "", 
-        "Tag":"ha",
-        "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "", "IsAnswer": true },
-            { "Id": 1056, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "", "IsAnswer": false },
-            { "Id": 1059, "QuestionId": 1010, "Name": "", "IsAnswer": true }],
-            "Explanation":"",
-            "Ref":""
-
-    },
-
+        {
+            "Id": 1,
+            "Name": "You are building a large, multi-tenant SaaS (software-as-a-service) application with a component that fetches data to process from a customer-specific Amazon S3 bucket in their account. How should you ensure that your application follows security best practices and limits risk when fetching data from customer-owned Amazon S3 buckets?",
+            "Tag": "security",
+            "Options": [
+                { "Id": 1055, "QuestionId": 1010, "Name": "Have users create an IAM user with a policy that grants read-only access to the Amazon S3 bucket required by your application, and store the corresponding access keys in an encrypted database that holds their account data.", "IsAnswer": false },
+                { "Id": 1056, "QuestionId": 1010, "Name": "Have users create a cross-account lAM role with a policy that grants read-only access to the Amazon S3 bucket required by your application to the AWS account ID running your production Sass application.", "IsAnswer": true },
+                { "Id": 1057, "QuestionId": 1010, "Name": "Have users create an Amazon S3 bucket policy that grants read-only access to the Amazon S3 bucket required by your application, and securely store the corresponding access keys in the database holding their account data.", "IsAnswer": false },
+                { "Id": 1058, "QuestionId": 1010, "Name": "Have users create an Amazon S3 bucket policy that grants read-only access to the Amazon S3 bucket required by your application and limits access to the public IP address of the SaaS application.", "IsAnswer": false }],
+            "Explanation": "See link for how to delegate access across AWS accounts using IAM roles",
+            "Ref": "http://docs.aws.amazon.com/IAM/latest/UserGuide/tutorial_cross-account-with-roles.html"
+        }
+,
       {
         "Id": 2,
         "Name": "", 
