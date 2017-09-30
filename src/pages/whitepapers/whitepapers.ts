91ecaea6b50fec80c61835191a90b83201abe195
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-09-30 12:02:55 -0500

    Updated whitepapers

--- src/pages/whitepapers/whitepapers.ts
+++ src/pages/whitepapers/whitepapers.ts
@@ -10,16 +10,20 @@ export class WhitepapersPage {
 
   whitepapers: Array<{ no: number, title: string, note: string,link:string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.whitepapers = [{no:1,title:"Security at Scale: Governance in AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Security_at_Scale_Governance_in_AWS_Whitepaper.pdf"},
-    {no:2,title:"Security at Scale: Logging in AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Security_at_Scale_Logging_in_AWS_Whitepaper.pdf"},
-    {no:3,title:"Securing Data at Rest with Encryption",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Securing_Data_at_Rest_with_Encryption.pdf"},
+    this.whitepapers = [
+    {no:1,title:"Securing Data at Rest with Encryption",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Securing_Data_at_Rest_with_Encryption.pdf"},
+    {no:2,title:"Web Application Hosting in the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-web-hosting-best-practices.pdf"},
+    {no:3,title:"Migrating AWS Resources to a New Region",note:"sdfsdfsd",link:"http://d0.awsstatic.com/whitepapers/aws-migrate-resources-to-new-region.pdf"},
     {no:4,title:"Development and Test on Amazon Web Services",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Development_Test_Environments.pdf"},
-    {no:5,title:"Operational Checklists for AWS",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Operational_Checklists.pdf"},
-    {no:6,title:"Architecting for the Cloud: Best Practices",note:"sdfsdfsd",link:"https://media.amazonwebservices.com/AWS_Cloud_Best_Practices.pdf"},
-    {no:7,title:"Building Fault-Tolerant Applications on AWS",note:"sdfsdfsd",link:"http://media.amazonwebservices.com/AWS_Building_Fault_Tolerant_Applications.pdf"},
-    {no:8,title:"Storage Options in the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
-    {no:9,title:"Backup and Recovery Approaches Using Amazon Web Services",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Backup_and_Recovery_Approaches_Using_AWS.pdf"},
-    {no:10,title:"AWS Security Best Practices",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Security/AWS_Security_Best_Practices.pdf"}];
+    {no:5,title:"Microservices on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/microservices-on-aws.pdf"},
+    {no:6,title:"Blue/Green Deployments on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Blue_Green_Deployments.pdf"},
+    {no:7,title:"Infrastructure as Code",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/DevOps/infrastructure-as-code.pdf"},
+    {no:8,title:"Introduction to DevOps on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_DevOps.pdf"},
+    {no:9,title:"Practicing Continuous Integration and Continuous Delivery on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/DevOps/practicing-continuous-integration-continuous-delivery-on-AWS.pdf"},
+    {no:10,title:"Development and Test on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-development-test-environments.pdf"},
+    {no:11,title:"Jenkins on AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/DevOps/Jenkins_on_AWS.pdf"},
+    {no:12,title:"Storage Options in the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
+    {no:13,title:"Import Windows Server to Amazon EC2 with PowerShell",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/DevOps/import-windows-server-to-amazon-ec2.pdf"}];
   }
 
   ionViewDidLoad() {
